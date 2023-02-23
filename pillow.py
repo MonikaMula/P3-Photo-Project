a1cfe8aa97bce3896f0a72b85d697c972cedc5a8
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2023-02-23 00:04:02 +0000

    Step One insert Picture

--- pillow.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-image = Image.open('Maxi.jpg')
-image.show()
-
-# 180 Image rotate Image up side down 🙃
-image = Image.open('Maxi.jpg')
-image_rotate = image.rotate(180)
-image_rotate.show()
