3050d88480e5636321d5025f3f2ad52e2618a751
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2023-02-26 14:44:49 +0000

    test

--- run.py
+++ run.py
@@ -1,32 +1,65 @@
-from PIL import Image, ImageColor
+from PIL import Image, ImageColor, ImageOps
+from urllib import request
 
-# First Step - Insert Picture or will be done automatically 
-a = input("Insert Photo, photo path You wish to Edit \n Insert/Paste Here:  ")
+# First Step - Insert Picture or will be done automatically
+url = input("Insert Photo, photo path You wish to Edit \n Insert/Paste Here: ")
 try:
-    image = Image.open(a)
+    request.urlretrieve(url, "Rework.png")
+    image = Image.open("Rework.png")
+    print("Picture was insert")
+    image_uploaded = True
 except:
-    image = Image.open('Maxi.jpg')
+    url_test = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/"\
+               "Python_logo_and_wordmark.svg/"\
+               "2560px-Python_logo_and_wordmark.svg.png"
+    request.urlretrieve(url_test, "Rework.png")
+    image = Image.open("Rework.png")
+    print("Picture was insert")
+    image_uploaded = True
     print("Picture wasn't insert, automatic upload picture as an example")
 
+# Image Rotation
+rotation = input('Would You Like Rotate An Image \n Yes/No: ')
+if rotation.lower().startswith("y"):
+  rotation_number = input(
+      'What degree You wish picture to rotate? \n Place degree number: ')
+  try:
+      image_rotate = image.rotate(int(rotation_number))
+      image_rotate.show()
+      image = image_rotate
+  except:
+      print("Go forward if You do not wish rotate an Image")
 
-#Image Rotation
-    rotation = input("Would You Like Rotate An Image \n Yes/No: ")
-if  rotation.lower().startswith("y"):
-    rotation_number = input("What degree You wish picture to rotate? \n Place degree number: ")
+# Grey Color
+grayscale = input("Would You like change picture to grey: \n Yes/No: ")
+if grayscale.lower().startswith("y"):
+  image = ImageOps.grayscale(image)
+  image.show()
+
+# Image_Cropping 
+cropping = input('Crop image? \n Yes/No: ')
+if cropping.lower().startswith("y"):
+    print(f"image size is: {image.size} ")
+    left_x = input("left: ")
+    top_y = input("top: ")
+    right_x = input("right: ")
+    bottom_y = input("bottom: ")
+    
 try:
-    image_rotate = image.rotate(int(rotation_number))
-    image_rotate.show()
-except:
-    print("Go forward if You do not wish rotate an Image")
-    
-    
-#Image_Cropping 
-    cropping = input("Crop image? \n Yes/No: ")
-if  cropping.lower().startswith("y"):
-try:   
-    image_crop = image_crop((left_x,top_y,right_x,bottom_y))
+    width, height = image.size
+    left = int(left_x)
+    top = int(top_y)
+    right = width - int(right_x)
+    bottom = height - int(bottom_y)
+    image_crop = image.crop((left, top, right, bottom))
     image_crop.show()
+    image = image_crop
 except:
-    print("Skip")
+    print("Wrong Input")
     
-    
+# Saving Image
+saving_mode = input("Would You like to save Your picture? \n Yes/No: ")
+if saving_mode.lower().startswith("y"):
+    save_path = input("Insert path where You wish to save picture: \n ")
+    if save_path:
+          image.save(f"{save_path}")    
